136219ba4f69c186b139f5652c13e7e5c0d77fb1
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-18 21:57:21 +0530

    committing files

---
+++ ml/regression.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+'''
+Neural Network Regression....
+
+How much will this house sell for?
+How many people will buy this app?
+How much will my health insurance be?
+How much should I save each week for fuel?
+
+Try to predict the coordinates like (13, 90) of where the box should be in a picture in an object detection problem
+'''
+
